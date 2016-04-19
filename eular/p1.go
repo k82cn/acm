f62e8a149a9eb1f4e25afd923b0b5edf5b83216a
Klaus Ma <klaus1982.cn@gmail.com>
2016-04-19 10:36:10 +0800

    Updated.

--- eular/p1.go
+++ eular/p1.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func main() {
     var s int;
-    var i int;
-    for i = 1; i < 1000; i++ {
+    for i := 1; i < 1000; i++ {
         if i % 3 == 0 || i % 5 == 0 {
             s += i;
         }
