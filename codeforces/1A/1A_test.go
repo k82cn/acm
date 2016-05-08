bea085ce82e3b09ac55846d44e43569bc21ac0d3
Klaus Ma <klaus1982.cn@gmail.com>
2016-05-08 22:08:15 +0800

    Use testing.Errorf

--- codeforces/1A/1A_test.go
+++ codeforces/1A/1A_test.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test_flagstone_case1(t *testing.T) {
 	res := flagstone(6, 6, 4)
 	if res != 4 {
-		t.Error("expected 4, but %d\n", res)
+		t.Errorf("expected 4, but %d\n", res)
 	}
 }
 
 func Test_flagstone_case2(t *testing.T) {
 	res := flagstone(1000000000, 1000000000, 1)
 	if res != 1000000000000000000 {
-		t.Error(fmt.Printf("expected 1000000000000000000, but %d\n", res))
+		t.Errorf("expected 1000000000000000000, but %d\n", res)
 	}
 }
